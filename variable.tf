30b06f91ecb8e08df7846aae63f48f0195d34078
jesusoctavioas <31106915+jesusoctavioas@users.noreply.github.com>
2025-05-26 07:49:03 -0700

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "test1"
+}
+
+variable "location" {
+  description = "eastus"
+  default     = "eastus"
+} 
